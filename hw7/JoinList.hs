690ab4d187c886be95d853669863f37e49c1ec8d
Raja Naresh <Raja.Naresh@corp.1010data.com>
2016-01-07 16:50:51 -0500

    Started hw7

---
+++ hw7/JoinList.hs
@@ -0,0 +1,6 @@
+{-# OPTIONS_GHC -Wall #-}
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
