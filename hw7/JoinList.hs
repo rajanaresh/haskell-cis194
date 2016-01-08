952d73ba2a8ec295a2cc2759de6b7166e696695e
Raja Naresh <Raja.Naresh@corp.1010data.com>
2016-01-08 16:20:32 -0500

    Sync

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -4,3 +4,10 @@ data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
+
+
+tag :: Monoid m => JoinList m a -> m
+tag (Single m a) = 
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+
