192253fa0d3a505271dd88ba3b3d73373a8b8b22
rajanaresh <rajanaresh89@gmail.com>
2016-01-10 20:57:02 -0500

    Implemented ex-1 in hw7

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -7,7 +7,8 @@ data JoinList m a = Empty
 
 
 tag :: Monoid m => JoinList m a -> m
-tag (Single m a) = 
+tag (Single m _)                               = mempty `mappend` m
+tag (Append _ (JoinList m1 _) (JoinList m2 _)) = tag m1 `mappend` tag m2
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-
+l +++ r = Append (tag l `mappend` tag r) l r
